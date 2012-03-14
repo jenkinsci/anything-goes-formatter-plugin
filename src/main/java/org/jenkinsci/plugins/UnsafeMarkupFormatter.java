7a078863862a8a7eccc18815a0316ac4eb31eadc
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-03-14 11:35:54 -0700

    Initial version

---
+++ src/main/java/org/jenkinsci/plugins/UnsafeMarkupFormatter.java
@@ -0,0 +1,31 @@
+package org.jenkinsci.plugins;
+
+import hudson.Extension;
+import hudson.markup.MarkupFormatter;
+import hudson.markup.MarkupFormatterDescriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class UnsafeMarkupFormatter extends MarkupFormatter {
+    @DataBoundConstructor
+    public UnsafeMarkupFormatter() {
+    }
+
+    @Override
+    public void translate(String markup, Writer output) throws IOException {
+        output.write(markup);
+    }
+
+    @Extension
+    public static class DescriptorImpl extends MarkupFormatterDescriptor {
+        @Override
+        public String getDisplayName() {
+            return "Allows arbitrary HTML includin JavaScript (UNSAFE)";
+        }
+    }
+}
